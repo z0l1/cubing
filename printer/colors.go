087be7ae75e1ad49dbe1ea5f55bfeaa5ce211061
z0l1 <andzo1998@gmail.com>
2024-02-18 17:55:12 +0100

    console colors

---
+++ printer/colors.go
@@ -0,0 +1,12 @@
+package printer
+
+const Reset = "\033[0m"
+const Red = "\033[31m"
+const Green = "\033[32m"
+const Yellow = "\033[33m"
+const Blue = "\033[34m"
+const Purple = "\033[35m"
+const Cyan = "\033[36m"
+const White = "\033[37m"
+
+const Orange = "\033[38;5;208m" // ANSI code for extended color (orange)
