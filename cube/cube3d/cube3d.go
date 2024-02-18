05b4a19aaba7be45a888f0492e74f9eb7536a321
z0l1 <andzo1998@gmail.com>
2024-02-18 17:57:36 +0100

    3d cube?

---
+++ cube/cube3d/cube3d.go
@@ -0,0 +1,10 @@
+package cube3d
+
+// cube as 3d obj. sounds more work
+// how would I mark multicolored cube objects.
+// demo sounds easier with "flat"
+
+type Cube [][][]string
+
+type Cube3d struct {
+}
