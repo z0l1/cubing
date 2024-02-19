f24e40f8ebf640ab6d2ac2a769191ae0765607cf
z0l1 <andzo1998@gmail.com>
2024-02-20 00:02:18 +0100

    getDirLoopInfo.go

--- cube/flatcube/getDirLoopInfo.go
+++ cube/flatcube/getDirLoopInfo.go
@@ -1,23 +1,23 @@
 package flatcube
 
-func getDirLoopInfo(_dir Direction) (start [2]int /*, end [2]int */, dir [2]int) {
+func getDirLoopInfo(_dir Direction) (start [2]int /*, end [2]int */, step [2]int) {
 	switch _dir {
 	case Top:
 		start = [2]int{0, 0}
 		//end = [2]int{0, 2}
-		dir = [2]int{0, 1}
+		step = [2]int{0, 1}
 	case Bottom:
 		start = [2]int{2, 2}
 		//end = [2]int{2, 0}
-		dir = [2]int{0, -1}
+		step = [2]int{0, -1}
 	case Left:
 		start = [2]int{2, 0}
 		//end = [2]int{0, 0}
-		dir = [2]int{-1, 0}
+		step = [2]int{-1, 0}
 	case Right:
 		start = [2]int{0, 2}
 		//end = [2]int{2, 2}
-		dir = [2]int{1, 0}
+		step = [2]int{1, 0}
 	}
 
 	return
