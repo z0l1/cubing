88bdd3990e039fc403513eb06557f2f87c4aebc1
z0l1 <andzo1998@gmail.com>
2024-02-20 00:02:10 +0100

    rotateneighbours.go ?

--- cube/flatcube/rotateneighbours.go
+++ cube/flatcube/rotateneighbours.go
@@ -1,15 +1,53 @@
 package flatcube
 
-func RotateNeighbours(sides *FlatCube, side SideColor, cc bool) {
-	//conns := getConnections(side)
+type SideRow [3]SideColor
 
-	//for _, conn := range conns {
-	//connStart, connDir := getDirLoopInfo(conn.dir)
-	//
-	//// tmp row
-	//var tmp [3]SideColor
-	//for {
-	//	a := _sides[0]
-	//}
-	//}
+func getRow(side *CubeSide, start [2]int, step [2]int) *SideRow {
+	var result SideRow
+	for i := 0; i < 3; i++ {
+		result[i] = side[start[0]][start[1]]
+
+		start[0] += step[0]
+		start[1] += step[1]
+	}
+
+	return &result
+}
+
+func writeRow(side *CubeSide, start [2]int, step [2]int, src *SideRow) {
+	for i := 0; i < 3; i++ {
+		side[start[0]][start[1]] = src[i]
+
+		start[0] += step[0]
+		start[1] += step[1]
+	}
+}
+
+func RotateNeighbours(sides *FlatCube, side SideColor, cc bool) {
+	conns := getConnections(side)
+
+	startI := 0
+	lastI := 3
+	step := 1
+	if cc {
+		startI = 3
+		lastI = 0
+		step = -1
+	}
+
+	// save last
+	lastConn := conns[lastI]
+	lastStart, lastStep := getDirLoopInfo(conns[lastI].dir)
+	swapper := getRow(&sides[lastConn.side], lastStart, lastStep)
+
+	for i := startI; i != lastI+step; i += step {
+		currConn := conns[i]
+		currStart, currStep := getDirLoopInfo(currConn.dir)
+		currSide := &sides[currConn.side]
+
+		tmp := getRow(currSide, currStart, currStep)
+		writeRow(currSide, currStart, currStep, swapper)
+		swapper = tmp
+	}
+
 }
